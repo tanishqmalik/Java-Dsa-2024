542a24b896b8f30024786c15975f2c069a65ae20
tanishqmalik <tanishqvideo@gmail.com>
2024-08-28 00:58:50 +0530

    rotateimageby 90

---
+++ 2darray/RotateImage.java
@@ -0,0 +1,30 @@
+public class RotateImage {
+    public static void main (String args[]){
+        int[][] arr= {{5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16}};
+
+        
+
+
+        // for(int i=0; i<arr.length; i++){
+        //     for(int j=0; j<arr[0].length; j++){
+        //         System.out.print(arr[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // }
+
+        int rotateImage[][] = new int[arr[0].length][arr.length];
+
+        for(int i=0; i<arr[0].length; i++){
+            for(int j=arr.length-1; j>=0; j--){
+                rotateImage[i][j]=arr[j][i];
+            }
+        }
+
+        for(int i=0; i<arr[0].length; i++){
+            for(int j=arr.length-1; j>=0; j--){
+                System.out.print(rotateImage[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
--- 2darray/transpose.java
+++ 2darray/transpose.java
@@ -1,31 +1,66 @@
+// import java.util.*;
+
+// public class transpose {
+//     public static void main(String[] args){
+//         int[][] a = {{1,2,3}, {4,5,6}};
+
+//         for(int i=0; i<a.length; i++){
+//             for(int j=0; j<a[0].length; j++){
+//                 System.out.print(a[i][j]+" ");
+//             }
+//             System.out.println();
+//         }
+
+
+//         //transpose
+
+//         int[][] transpose=new int[a[0].length][a.length];
+
+
+//         for(int i=0; i<a[0].length; i++){
+//             for(int j=0; j<a.length; j++){
+//                 transpose[i][j]= a[j][i];
+//             }
+//         }
+
+
+//         for(int i=0; i<a[0].length; i++){
+//             for(int j=0; j<a.length; j++){
+//                 System.out.print(transpose[i][j]);
+//             }
+
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
 import java.util.*;
 
-public class transpose {
-    public static void main(String[] args){
-        int[][] a = {{1,2,3}, {4,5,6}};
+public class transpose{
+    public static void main(String args[]){
+        int[][] arr = {{1,2,3}, {4,5,6}, {7,8,9}};
 
-        for(int i=0; i<a.length; i++){
-            for(int j=0; j<a[0].length; j++){
-                System.out.print(a[i][j]+" ");
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[0].length; j++){
+                System.out.print(arr[i][j]+" ");
             }
             System.out.println();
         }
 
-
-        //transpose
-
-        int[][] transpose=new int[a[0].length][a.length];
+        int[][] transpose = new int[arr[0].length][arr.length];
 
 
-        for(int i=0; i<a[0].length; i++){
-            for(int j=0; j<a.length; j++){
-                transpose[i][j]= a[j][i];
+        for(int i=0; i<arr[0].length; i++){
+            for(int j=0; j<arr.length; j++){
+                transpose[i][j]=arr[j][i];
             }
+            // System.out.println();
         }
 
-
-        for(int i=0; i<a[0].length; i++){
-            for(int j=0; j<a.length; j++){
+                for(int i=0; i<arr[0].length; i++){
+            for(int j=0; j<arr.length; j++){
                 System.out.print(transpose[i][j]);
             }
 
